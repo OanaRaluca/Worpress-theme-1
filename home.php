af75c70a1fcd8bbfbdaa6b7bfcebf50bf22eaabf
OanaRaluca <oana.petre14@yahoo.com>
2015-08-20 12:08:39 +0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php  get_header(); ?>
+<!-- START CONTENT -->  
+
+<div id="main"> 
+
+<div id="content">
+
+ <h2>Main Area</h2> 
+
+</div>
+
+<div id="sidebar">
+
+<?php  get_sidebar(); ?>;
+
+ </div>
+
+</div> 
+
+<div id="delimiter">
+
+</div>  
+
+<!-- END CONTENT -->
